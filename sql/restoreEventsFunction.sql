e11ba1d6dc9428b46d1a37d5b881a9f2b01b6902
Don Gaudino <don@panosoft.com>
2016-07-21 14:37:49 -0700

    Refactored variable names.  Added consistency checks for restored Source database.

--- sql/restoreEventsFunction.sql
+++ sql/restoreEventsFunction.sql
@@ -1,16 +1,16 @@
-CREATE FUNCTION restore_events(fromHost text, fromDatabase text, "user" text, "password" text, OUT rowsRestored bigint, OUT nextInsertId bigint)
+CREATE FUNCTION restore_events(fromHost text, fromDatabase text, fromDatabaseUser text, fromDatabasePassword text, OUT rows_restored bigint, OUT next_insert_id bigint)
 	AS $$
 DECLARE
 	getEventsStmt CONSTANT text = 'SELECT id, ts, entity_id, event FROM events ORDER BY id';
 	getCountMaxIdStmt CONSTANT text = 'SELECT MAX(id) AS maxid, count(*) AS count FROM events';
 	connectionInfo text;
 	sourceEventsCount bigint;
+	sourceEventsMaxId bigint;
 	sourceNextIdValue bigint;
 	fromEventsCount bigint;
 	fromEventsMaxId bigint;
 BEGIN
-	connectionInfo := 'host=' || fromHost || ' dbname=' || fromDatabase || ' user=' || "user" || ' password=' || "password";
-	RAISE NOTICE 'connection string %', connectionInfo;
+	connectionInfo := 'host=' || fromHost || ' dbname=' || fromDatabase || ' user=' || fromDatabaseUser || ' password=' || fromDatabasePassword;
 	-- get row count from Source events table.  must be 0 after being created by slate-init-db.
 	SELECT count(*) from events into sourceEventsCount;
 	IF sourceEventsCount != 0 THEN
@@ -35,10 +35,16 @@ BEGIN
 		SELECT fe.id, fe.ts, fe.entity_id, fe.event
 			FROM dblink(connectionInfo, getEventsStmt)
 			AS fe(id bigint, ts timestamp with time zone, entity_id uuid, event jsonb);
-	GET DIAGNOSTICS rowsRestored = ROW_COUNT;
+	GET DIAGNOSTICS rows_restored = ROW_COUNT;
+	SELECT MAX(id), count(*) FROM events INTO sourceEventsMaxId, sourceEventsCount;
+	IF sourceEventsCount != sourceEventsMaxId THEN
+		RAISE EXCEPTION 'The Source events table row count (%) is not equal to the Source events table maximum id (%)', sourceEventsCount, sourceEventsMaxId USING HINT = 'The restored Source events table is not valid';
+	END IF;
+	IF sourceEventsCount != rows_restored THEN
+		RAISE EXCEPTION 'The Source events table row count (%) is not equal to the Source events table rows restored (%)', sourceEventsCount, rows_restored USING HINT = 'The Source events table restore had a program logic error';
+	END IF;
 	-- update the id value in the Source id table to the maximum id value + 1 from the remote events table used to restore the Source events table.
-	UPDATE id SET id = fromEventsMaxId + 1 RETURNING id INTO nextInsertId;
-	RAISE NOTICE 'rows restored %  next start id %', rowsRestored, nextInsertId;
+	UPDATE id SET id = sourceEventsMaxId + 1 RETURNING id INTO next_insert_id;
 	-- the count of events copied to the Source events table and the next events table id value to be used for the next event inserted into the Source events table are returned.
 END;
 $$ LANGUAGE plpgsql;
