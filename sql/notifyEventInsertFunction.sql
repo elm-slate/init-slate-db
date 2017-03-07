c636433b0579e5a9f37586d59a7dd4f512000268
Don Gaudino <don@panosoft.com>
2017-03-07 10:44:25 -0800

    Added event column to notify.

--- sql/notifyEventInsertFunction.sql
+++ sql/notifyEventInsertFunction.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION notify_event_insert()
 	RETURNS TRIGGER AS $$
 DECLARE
 BEGIN
-	PERFORM pg_notify('eventsinsert', json_build_object('table', TG_TABLE_NAME, 'id', NEW.id )::text);
+	PERFORM pg_notify('eventsinsert', json_build_object('table', TG_TABLE_NAME, 'id', NEW.id, 'event', NEW.event )::text);
 	RETURN new;
 END;
 $$ LANGUAGE plpgsql;
